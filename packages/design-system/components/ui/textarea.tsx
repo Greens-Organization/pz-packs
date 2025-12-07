b97296fa7b117c9534c052ad53bb05d0f0749977
Matheus Agostinho <contact@matheuspa.com>
2025-12-07 19:00:42 -0300

    feat: refactor InputGroup, Input, Textarea components for improved styling and functionality; add Select component with various subcomponents

--- packages/design-system/components/ui/textarea.tsx
+++ packages/design-system/components/ui/textarea.tsx
@@ -1,14 +1,13 @@
-import * as React from "react"
+import { cn } from '@org/design-system/lib/utils'
+import type * as React from 'react'
 
-import { cn } from "@org/design-system/lib/utils"
-
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
+function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
   return (
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
+        'px-4 py-2 w-full border-2 rounded border-border shadow-md transition focus:outline-hidden focus:shadow-xs placeholder:text-muted-foreground',
+        className,
       )}
       {...props}
     />
