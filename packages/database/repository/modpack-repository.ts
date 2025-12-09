efe74f479d81fa3fc007fc1dc73ad2ae4f8e7ff2
Matheus Agostinho <contact@matheuspa.com>
2025-12-08 21:32:25 -0300

    feat(modpacks): include active members filtering in modpack retrieval

--- packages/database/repository/modpack-repository.ts
+++ packages/database/repository/modpack-repository.ts
@@ -300,6 +300,11 @@ export class ModpackRepository {
       orderBy: orderByFn(orderByColumn),
       limit,
       offset,
+      with: {
+        members: {
+          where: eq(modpacksMembers.isActive, true),
+        },
+      },
     })
 
     return {
