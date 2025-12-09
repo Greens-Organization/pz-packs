56d9548cac9241b8401ce06c91fac0c9286d3007
Matheus Agostinho <contact@matheuspa.com>
2025-12-09 12:50:25 -0300

    Refactor code structure for improved readability and maintainability

--- apps/web/src/components/header/app-header.tsx
+++ apps/web/src/components/header/app-header.tsx
@@ -1,15 +1,25 @@
+import { useTheme } from '@org/design-system/providers'
 import { Link } from '@tanstack/react-router'
 import { Anchor } from '../anchor'
 import { NavUser } from './nav-user'
 
 export function AppHeader() {
+  const { theme } = useTheme()
   return (
     <header className="container flex justify-between items-center my-4">
       <Link
         to="/"
         className="hover:scale-95 active:scale-110 transition-all duration-200 ease-in-out flex flex-row gap-2 items-center"
       >
-        <img src="/favicon.svg" alt="PZ Packs" className="h-16" />
+        <img
+          src={
+            theme === 'dark'
+              ? '/brand/pz-packs-logo-dark.svg'
+              : '/brand/pz-packs-logo.svg'
+          }
+          alt="PZ Packs"
+          className="h-16"
+        />
         <h1 className="font-bold text-2xl">PZ Packs</h1>
       </Link>
       <nav className="flex flex-row gap-3 items-center">
