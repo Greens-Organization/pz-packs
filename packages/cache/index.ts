e1fa5f8a788f8861427dd41ffc870323d5b57054
Matheus Agostinho <contact@matheuspa.com>
2025-12-22 21:42:33 -0300

    fix: replace RedisClient with ioredis for cache client initialization

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -1,6 +1,4 @@
-import { RedisClient } from 'bun'
+import Redis from 'ioredis'
 import { env } from './env'
 
-export const cacheClient = new RedisClient(env.SECONDARY_DATABASE_URL, {
-  tls: true,
-})
+export const cacheClient = new Redis(env.SECONDARY_DATABASE_URL)
